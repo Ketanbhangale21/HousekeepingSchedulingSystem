6d00766655c4d407e03b98a9ef3aef42f5abe1ce
Dhanush-Amudalapalli <dhanushamudalapalli16@gmail.com>
2024-02-10 11:28:48 +0530

    new commit

--- frontend/src/components/AdminDashboard/Feedback/Feedback.js
+++ frontend/src/components/AdminDashboard/Feedback/Feedback.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './AFeedback.css';
+import './Feedback.css';
 
 
 
@@ -7,11 +7,11 @@ import './AFeedback.css';
 function Feedback(){
     const DataArray = [
         {Fid: "01",Req_Id:"221",SName:"Dhanush",Fb:"Cleaning is good",Ratings:"4"},
-        {Fid: "02",Req_Id:"112",SName:"kethan",Fb:"Room service is good",Ratings:"5"},
+        {Fid: "02",Req_Id:"112",SName:"ketan",Fb:"Room service is good",Ratings:"5"},
         {Fid: "03",Req_Id:"125",SName:"Mayur",Fb:"Needs to be improve,but its okk",Ratings:"4"},
         {Fid: "04",Req_Id:"254",SName:"Maneesha",Fb:"Excellent work",Ratings:"4"},
         {Fid: "05",Req_Id:"220",SName:"Monika",Fb:"Good Work",Ratings:"5"},
-        {Fid: "06",Req_Id:"115",SName:"Shivaranjini",Fb:"good Service",Ratings:"5"},
+        {Fid: "06",Req_Id:"115",SName:"Shivaranjani",Fb:"good Service",Ratings:"5"},
     ];
 
     let resultArray = DataArray.map((item) => {
